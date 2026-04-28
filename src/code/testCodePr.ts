c0d7e52e36c9adddd26c1b082aaa6d333cdc4adb
Emerson-feral <emerson_feral@hotmail.com>
2026-04-27 21:07:31 -0300

    Bad code example

---
+++ src/code/testCodePr.ts
@@ -0,0 +1,37 @@
+// userService.ts
+
+import express from "express";
+
+const app = express();
+
+let users: any = [];
+
+app.get("/users", async (req, res) => {
+  try {
+    const data = await fetch("https://api.externa.com/users");
+    const json = await data.json();
+
+    json.map((u: any) => {
+      users.push(u);
+    });
+
+    res.send(users);
+  } catch (e) {
+    console.log(e);
+    res.send("erro");
+  }
+});
+
+app.post("/users", (req, res) => {
+  const body = req.body;
+
+  if (!body.name) {
+    res.send("name required");
+  }
+
+  users.push(body);
+
+  res.send(users);
+});
+
+export default app;
