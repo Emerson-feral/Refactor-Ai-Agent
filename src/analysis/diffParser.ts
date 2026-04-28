4fc4a2bce39f8f6396c0906d88e77cd65348a003
Emerson-feral <emerson_feral@hotmail.com>
2026-04-28 19:11:33 -0300

    Remove unused splitDiff function from diffParser.ts

--- src/analysis/diffParser.ts
+++
@@ -1,9 +0,0 @@
-export function splitDiff(diff: string, maxLength = 2000): string[] {
-  const chunks: string[] = [];
-
-  for (let i = 0; i < diff.length; i += maxLength) {
-    chunks.push(diff.slice(i, i + maxLength));
-  }
-
-  return chunks;
-}
